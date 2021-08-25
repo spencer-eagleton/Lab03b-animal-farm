02e154d822903d3efda559112bfef5713c6201c4
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-08-25 16:55:29 -0700

    created the mythical beast farm

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 // import functions
+const catSound = document.getElementById('cat-audio');
+const catImage = document.getElementById('cat-image');
+
+const dogSound = document.getElementById('dog-audio');
+const dogImage = document.getElementById('dog-image');
+
+const horseSound = document.getElementById('horse-audio');
+const horseImage = document.getElementById('horse-image');
+
+
+
+
+
 
 // reference needed DOM elements
+catImage.addEventListener('click', () => {
+    
 
+    catSound.play();
 
+});
+
+dogImage.addEventListener('click', () => {
+    dogSound.play();
+});
+
+horseImage.addEventListener('click', () => {
+    horseSound.play();
+});
 // set event listeners 
   // get user input(s)
   // do any needed work with the value(s)
